eb9299e464c792006ca2f5e678a770bc7d84f81d
Marcondezz <igorpmarcondes@yahoo.com>
2024-06-08 14:47:00 -0300

    Finalização

--- src/app/sucesso/page.tsx
+++ src/app/sucesso/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import "./CSS/sucesso.css"
 import imagens from "./navbar-sucesso.json"
+import fotoSucesso from "./../../../public/assets/sucesso/complete.png"
 
 interface dados{
     id: number;
@@ -13,9 +14,11 @@ const sucesso = () =>{
             <section className="OrgTotal_sucesso">
                     <div className="Org-sucesso">
                         <div className="edicao-sucesso">
-                        <h2 className="texto-sucesso">Informações enviadas com sucesso</h2>
-                        <h3 className="texto-questionario">Faça o teste agora mesmo!</h3>
-                        </div>
+                            <h2 className="texto-sucesso">Informações enviadas com sucesso</h2>
+                            <img className="bolinhaSucesso" src={ fotoSucesso.src }/>
+                            </div>
+                            <h4 className="textodois-sucesso">Retornaremos com a resposta em breve</h4>
+                            <h3 className="textodois-sucesso">Enquanto aguarda o retorno, confira as opções recomendadas para suas características:</h3>
                         <div>
                             <div className="layout-paginaInicial" >
                             {imagens.map ((item2 : dados) =>(
